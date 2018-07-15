01494e9defcb3315e416e9789506df29679ebd9b
Wei Huang <Whuang@1800flowers.com>
2018-07-15 18:33:56 -0400

    Init

---
+++ src/Sum8.java
@@ -0,0 +1,21 @@
+
+public class Sum8 {
+
+	public static void main(String[] args) {
+		int myList[] = {1, 2, 3, 3};
+		int len = myList.length-1;
+		int iteration = 0;
+		int count = 0;
+		for (int i = 0; i <= len; i++) 
+		{
+			iteration++;
+			System.out.println("Iteration+++++++++++++ "+iteration+" +++++++++++++++++++++++");
+			for (int j = i+1; j <= len; j++)
+			{
+				System.out.println(myList[i]+myList[j]);
+				count++;
+				System.out.println("Count "+count);
+			}
+		}
+	}
+}
