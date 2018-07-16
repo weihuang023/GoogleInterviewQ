14a7ae1da4ca9af612f532237c7771939337bf23
Wei Huang <Whuang@1800flowers.com>
2018-07-15 22:41:07 -0400

    Add SUM 8

--- src/Sum8.java
+++ src/Sum8.java
@@ -1,21 +1,49 @@
 
 public class Sum8 {
 
-	public static void main(String[] args) {
-		int myList[] = {1, 2, 3, 3};
-		int len = myList.length-1;
+	public void addNum(int myList[]) {
 		int iteration = 0;
 		int count = 0;
+		int len = myList.length-1;
 		for (int i = 0; i <= len; i++) 
 		{
 			iteration++;
-			System.out.println("Iteration+++++++++++++l "+iteration+" +++++++++++++++++++++++");
+			System.out.println("Iteration +++++++++++++ "+iteration+" +++++++++++++++++++++++");
 			for (int j = i+1; j <= len; j++)
 			{
-				System.out.println(myList[i]+myList[j]);
+				System.out.println("Add Two Num: " +(myList[i]+myList[j]));
 				count++;
 				System.out.println("Count "+count);
 			}
+			System.out.println("========= +++++++++++++ "+iteration+" +++++++++++++++++++++++");
 		}
+	}
+	
+	public int foundLess8(int myList[]) {
+		int getLess8 = 0;
+		
+		for (int i = 0; i<= myList.length -1; i++)
+		{
+			if (myList[i] <= 8) 
+			{
+				//System.out.println("Found Num Less than 8: " +myList[i]);
+				getLess8 = myList[i];
+			}
+			else
+			{
+				//System.out.println("No Num found...................");
+			}
+		}
+		return getLess8;
+	}
+	
+	public static void main(String[] args) {
+		Sum8 sum = new Sum8();
+		int myList[] = {1, 2, 3, 3, 9, 9, 9, 9, 1, 9};
+		//sum.addNum(myList);
+		
+		sum.foundLess8(myList);
+		
+		
 	}
 }
